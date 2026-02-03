76622512ed9d99bfdcaec5e2814121aa7c5a1370
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-02-03 20:29:04 +0000

    This most definitely doesn't work.

--- Registration.cs
+++ Registration.cs
@@ -2,4 +2,13 @@
 public class Registration
 {
     // Add your code here
+    public bool oldEnough = true;
+    public string ValidateAge(oldEnough)
+    {
+        if (oldEnough)
+        {
+            return "old enough";
+        } 
+        return "";
+    }
 }
